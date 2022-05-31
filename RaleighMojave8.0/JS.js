90fb5828f175860fcaf5a7d721f77e3c889d76f4
Denis982022 <janop.98.15@gmail.com>
2022-05-31 18:23:42 -0300

    carrrousel

--- RaleighMojave8.0/JS.js
+++ RaleighMojave8.0/JS.js
@@ -1,80 +1,40 @@
-window.onload = function(){
-    //variables
-    const IMAGENES = [
-        'imgs_raleigh/raleigh.png',  
-        'imgs_raleigh/raleigh_bin8.jpg'     ];
-        const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
-        let posicionActual = 0;
-        let $botonRetroceder = document.querySelector('#retroceder');
-    let $botonAvanzar = document.querySelector('#avanzar');
-    let $imagen = document.querySelector('#imagen');
-    let $botonPlay = document.querySelector('#play');
-    let $botonStop = document.querySelector('#stop');
-    let intervalo;
+var rand1 =1;
 
-    // Funciones
+function camRetroceder()
+{
+var imagenumber = 2 ;
 
-    /**
-     * Funcion que cambia la foto en la siguiente posicion
-     */
-    function pasarFoto() {
-        if(posicionActual >= IMAGENES.length - 1) {
-            posicionActual = 0;
-        } else {
-            posicionActual++;
-        }
-        renderizarImagen();
-    }
 
-    /**
-     * Funcion que cambia la foto en la anterior posicion
-     */
-    function retrocederFoto() {
-        if(posicionActual <= 0) {
-            posicionActual = IMAGENES.length - 1;
-        } else {
-            posicionActual--;
-        }
-        renderizarImagen();
-    }
+images = new Array;
+images[1] = "imgs-raleigh/raleigh.png";
+images[2] = "imgs-raleigh/raleigh_bin8.jpg";
 
-    /**
-     * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
-     */
-    function renderizarImagen () {
-        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
-    }
 
-    /**
-     * Activa el autoplay de la imagen
-     */
-    function playIntervalo() {
-        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
-        // Desactivamos los botones de control
-        $botonAvanzar.setAttribute('disabled', true);
-        $botonRetroceder.setAttribute('disabled', true);
-        $botonPlay.setAttribute('disabled', true);
-        $botonStop.removeAttribute('disabled');
+ rand1 -= 1;
+  if(rand1 < 1){
+    rand1 = 3;
+}   
 
-    }
 
-    /**
-     * Para el autoplay de la imagen
-     */
-    function stopIntervalo() {
-        clearInterval(intervalo);
-        // Activamos los botones de control
-        $botonAvanzar.removeAttribute('disabled');
-        $botonRetroceder.removeAttribute('disabled');
-        $botonPlay.removeAttribute('disabled');
-        $botonStop.setAttribute('disabled', true);
-    }
+document.miimagen.src= images[rand1];
+}
 
-    // Eventos
-    $botonAvanzar.addEventListener('click', pasarFoto);
-    $botonRetroceder.addEventListener('click', retrocederFoto);
-    $botonPlay.addEventListener('click', playIntervalo);
-    $botonStop.addEventListener('click', stopIntervalo);
-    // Iniciar
-    renderizarImagen();
+function camAvanzar()
+{
+var imagenumber = 2 ;
+
+
+images = new Array;
+images[1] = "imgs-raleigh/raleigh.png";
+images[2] = "imgs-raleigh/raleigh_bin8.jpg";
+
+
+ rand1 += 1;
+  if(rand1 > 3){
+    rand1 = 1;
+}   
+
+
+
+document.miimagen.src= images[rand1];
 }

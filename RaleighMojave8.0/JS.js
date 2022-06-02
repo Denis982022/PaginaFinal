65b846abd84a83ecb2b5e6908c88d85aa006fd49
Denis982022 <janop.98.15@gmail.com>
2022-06-02 18:38:31 -0300

    carrouselfinal

--- RaleighMojave8.0/JS.js
+++ RaleighMojave8.0/JS.js
@@ -1,40 +1,38 @@
+
 var rand1 =1;
 
 function camRetroceder()
 {
-var imagenumber = 2 ;
 
 
 images = new Array;
-images[1] = "imgs-raleigh/raleigh.png";
-images[2] = "imgs-raleigh/raleigh_bin8.jpg";
-
-
+images[1] = "imgs-raleigh/raleigh_1.jpg";
+images[2] = "imgs-raleigh/raleigh_2.jpg";
+images[3] = "imgs-raleigh/raleigh_3.jpg";
+images[4] = "imgs-raleigh/raleigh_4.jpg";
  rand1 -= 1;
   if(rand1 < 1){
-    rand1 = 3;
+    rand1 = 4;
 }   
-
 
 document.miimagen.src= images[rand1];
 }
 
 function camAvanzar()
 {
-var imagenumber = 2 ;
 
 
 images = new Array;
-images[1] = "imgs-raleigh/raleigh.png";
-images[2] = "imgs-raleigh/raleigh_bin8.jpg";
-
+images[1] = "imgs-raleigh/raleigh_1.jpg";
+images[2] = "imgs-raleigh/raleigh_2.jpg";
+images[3] = "imgs-raleigh/raleigh_3.jpg";
+images[4] = "imgs-raleigh/raleigh_4.jpg";
 
  rand1 += 1;
-  if(rand1 > 3){
+  if(rand1 > 4){
     rand1 = 1;
 }   
-
-
-
+setTimeout("camAvanzar()", 7000)
 document.miimagen.src= images[rand1];
 }
+rand1 = setTimeout("camAvanzar()", 7000)
